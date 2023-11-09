584982addfe4f0a7c94c05ea2668e2e1dde8ad17
trevor dewindt <trevor.dewindt@gmail.com>
2023-11-09 01:19:00 -0700

    add final code

--- server/models.py
+++ server/models.py
@@ -11,13 +11,42 @@ class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    birthday = db.Column(db.Date)
+    
+    animals = db.relationship('Animal', backref='zookeeper')
+
+    def __repr__(self):
+        return f'Zookeeper(id={self.id}, ' + \
+            f'name={self.name}, ' + \
+            f'birthday={self.birthday})'
 
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String)
+    open_to_visitors = db.Column(db.Boolean)
+
+    animals = db.relationship('Animal', backref='enclosure')
+
+    def __repr__(self):
+        return f'Enclosure(id={self.id}, ' + \
+            f'name={self.environment}, ' + \
+            f'open_to_visitors={self.open_to_visitors})'
+
 
 class Animal(db.Model):
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    species = db.Column(db.String)
+    
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeepers.id'))
+    enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosures.id'))
+
+    def __repr__(self):
+        return f'Animal(id={self.id}, ' + \
+            f'name={self.name}, ' + \
+            f'species={self.species})'
